ff85f3b15275465fb65a4155f69c4b3e5ba8dc37
Hammad Ali <hammadaali02@gmail.com>
2024-08-10 17:13:04 +0500

    completed

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,3 +1,72 @@
+// import 'package:flutter/material.dart';
+
+// class HomeScreen extends StatefulWidget {
+//   const HomeScreen({super.key});
+
+//   @override
+//   State<HomeScreen> createState() => _HomeScreenState();
+// }
+
+// final List<String> _products = [
+//   'Apple 🍎',
+//   'Mango 🥭',
+//   'Orange 🍊',
+//   'Banana 🍌',
+//   'Strawbery 🍓',
+//   'Cherry 🍒'
+// ];
+
+// class _HomeScreenState extends State<HomeScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text(
+//           'Reorderable ListView',
+//           style: TextStyle(
+//             color: Colors.white,
+//             fontWeight: FontWeight.bold,
+//           ),
+//         ),
+//         backgroundColor: Colors.deepOrangeAccent,
+//       ),
+//       body: ReorderableListView.builder(
+//         padding: EdgeInsets.all(10),
+//         physics: BouncingScrollPhysics(),
+//           itemBuilder: (context, index) {
+//             return Container(
+//               key: ValueKey(_products[index]),
+//               height: 100,
+//               width: double.maxFinite,
+//               decoration: BoxDecoration(
+//                 color: Colors.deepOrangeAccent,
+//                 borderRadius: BorderRadius.circular(10),
+//               ),
+//               child: Center(
+//                 child: Text(
+//                   _products[index],
+//                   style: TextStyle(
+//                     color: Colors.white,
+//                     fontWeight: FontWeight.bold,
+//                   ),
+//                 ),
+//               ),
+//             );
+//           },
+//           itemCount: _products.length,
+//           onReorder: (oldIndex, newIndex) {
+//             setState(() {
+//               if (newIndex > oldIndex) {
+//                 newIndex = newIndex - 1;
+//               }
+//               final element = _products.removeAt(oldIndex);
+//               _products.insert(newIndex, element);
+//             });
+//           }),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -6,6 +75,15 @@ class HomeScreen extends StatefulWidget {
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
+
+final List<String> _products = [
+  'Apple 🍎',
+  'Mango 🥭',
+  'Orange 🍊',
+  'Banana 🍌',
+  'Strawberry 🍓',
+  'Cherry 🍒',
+];
 
 class _HomeScreenState extends State<HomeScreen> {
   @override
@@ -20,6 +98,51 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
         backgroundColor: Colors.deepOrangeAccent,
+      ),
+      body: ReorderableListView.builder(
+        physics: const BouncingScrollPhysics(),
+        itemBuilder: (context, index) {
+          return Padding(
+            key: ValueKey(_products[index]),
+            padding: const EdgeInsets.all( 10.0),
+            child: Container(
+              height: 80,
+              width: double.maxFinite,
+              decoration: BoxDecoration(
+                color: Colors.deepOrangeAccent,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 10,
+                    spreadRadius: 1,
+                    offset: Offset(0, 5),
+                  ),
+                ],
+              ),
+              child: Center(
+                child: Text(
+                  _products[index],
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+        itemCount: _products.length,
+        onReorder: (oldIndex, newIndex) {
+          setState(() {
+            if (newIndex > oldIndex) {
+              newIndex = newIndex - 1;
+            }
+            final element = _products.removeAt(oldIndex);
+            _products.insert(newIndex, element);
+          });
+        },
       ),
     );
   }
