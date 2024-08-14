8e00902cc2adf7a5cc34d90ac78e8ea03031ae8f
Hammad Ali <hammadaali02@gmail.com>
2024-08-14 23:16:50 +0500

    updated

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_reorderable_listview/models/user.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -7,73 +8,166 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-final List<String> _products = [
-  'Apple 🍎',
-  'Mango 🥭',
-  'Orange 🍊',
-  'Banana 🍌',
-  'Strawberry 🍓',
-  'Cherry 🍒',
+List<User> _users = [
+  User(
+      'Elliana Palacios',
+      '@elliana',
+      'https://images.unsplash.com/photo-1504735217152-b768bcab5ebc?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=0ec8291c3fd2f774a365c8651210a18b',
+      false),
+  User(
+      'Kayley Dwyer',
+      '@kayley',
+      'https://images.unsplash.com/photo-1503467913725-8484b65b0715?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=cf7f82093012c4789841f570933f88e3',
+      false),
+  User(
+      'Kathleen Mcdonough',
+      '@kathleen',
+      'https://images.unsplash.com/photo-1507081323647-4d250478b919?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b717a6d0469694bbe6400e6bfe45a1da',
+      false),
+  User(
+      'Kathleen Dyer',
+      '@kathleen',
+      'https://images.unsplash.com/photo-1502980426475-b83966705988?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=ddcb7ec744fc63472f2d9e19362aa387',
+      false),
+  User(
+      'Mikayla Marquez',
+      '@mikayla',
+      'https://images.unsplash.com/photo-1541710430735-5fca14c95b00?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
+      false),
+  User(
+      'Kiersten Lange',
+      '@kiersten',
+      'https://images.unsplash.com/photo-1542534759-05f6c34a9e63?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
+      false),
+  User(
+      'Carys Metz',
+      '@metz',
+      'https://images.unsplash.com/photo-1516239482977-b550ba7253f2?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
+      false),
+  User(
+      'Ignacio Schmidt',
+      '@schmidt',
+      'https://images.unsplash.com/photo-1542973748-658653fb3d12?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
+      false),
+  User(
+      'Clyde Lucas',
+      '@clyde',
+      'https://images.unsplash.com/photo-1569443693539-175ea9f007e8?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
+      false),
+  User(
+      'Mikayla Marquez',
+      '@mikayla',
+      'https://images.unsplash.com/photo-1541710430735-5fca14c95b00?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
+      false)
 ];
 
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey.shade900,
       appBar: AppBar(
-        title: const Text(
-          'Reorderable ListView',
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
+        elevation: 0,
+        backgroundColor: Colors.grey.shade900,
+        title: Container(
+          height: 38,
+          child: TextField(
+            style: TextStyle(color: Colors.white),
+            decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.grey[850],
+                contentPadding: EdgeInsets.all(0),
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: Colors.grey.shade500,
+                ),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50),
+                    borderSide: BorderSide.none),
+                hintStyle: TextStyle(fontSize: 14, color: Colors.grey.shade500),
+                hintText: "Search users"),
           ),
         ),
-        backgroundColor: Colors.deepOrangeAccent,
       ),
       body: ReorderableListView.builder(
         physics: const BouncingScrollPhysics(),
         itemBuilder: (context, index) {
-          return Padding(
-            key: ValueKey(_products[index]),
-            padding: const EdgeInsets.all( 10.0),
-            child: Container(
-              height: 80,
-              width: double.maxFinite,
-              decoration: BoxDecoration(
-                color: Colors.deepOrangeAccent,
-                borderRadius: BorderRadius.circular(20),
-                boxShadow: const [
-                  BoxShadow(
-                    color: Colors.black26,
-                    blurRadius: 10,
-                    spreadRadius: 1,
-                    offset: Offset(0, 5),
-                  ),
-                ],
-              ),
-              child: Center(
-                child: Text(
-                  _products[index],
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                  ),
-                ),
-              ),
-            ),
+          return userComponent(
+            user: _users[index],
+            key: ValueKey(_users[index]),
           );
         },
-        itemCount: _products.length,
+        itemCount: _users.length,
         onReorder: (oldIndex, newIndex) {
           setState(() {
             if (newIndex > oldIndex) {
               newIndex = newIndex - 1;
             }
-            final element = _products.removeAt(oldIndex);
-            _products.insert(newIndex, element);
+            final element = _users.removeAt(oldIndex);
+            _users.insert(newIndex, element);
           });
         },
+      ),
+    );
+  }
+
+  userComponent({required User user, required ValueKey key}) {
+    return Container(
+      key: key,
+      margin: EdgeInsets.symmetric(horizontal: 20),
+      padding: EdgeInsets.only(top: 10, bottom: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(children: [
+            Container(
+                width: 60,
+                height: 60,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(50),
+                  child: Image.network(user.image),
+                )),
+            SizedBox(width: 10),
+            Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Text(user.name,
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.w500)),
+              SizedBox(
+                height: 5,
+              ),
+              Text(user.username, style: TextStyle(color: Colors.grey[500])),
+            ])
+          ]),
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                user.isFollowedByMe = !user.isFollowedByMe;
+              });
+            },
+            child: AnimatedContainer(
+              height: 35,
+              width: 110,
+              duration: Duration(milliseconds: 300),
+              decoration: BoxDecoration(
+                  color:
+                      user.isFollowedByMe ? Colors.blue[700] : Color(0xffffff),
+                  borderRadius: BorderRadius.circular(5),
+                  border: Border.all(
+                    color: user.isFollowedByMe
+                        ? Colors.transparent
+                        : Colors.grey.shade700,
+                  )),
+              child: Center(
+                child: Text(
+                  user.isFollowedByMe ? 'Unfollow' : 'Follow',
+                  style: TextStyle(
+                    color: user.isFollowedByMe ? Colors.white : Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -14,10 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Reorderable ListView',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      // theme: ThemeData.dark(),
       home: const HomeScreen(),
     );
   }
